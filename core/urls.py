94213d205b1b8e670f40a87b496e6910c006ad69
AdarshVarma9395 <adarshvarma1999@gmail.com>
2023-12-28 16:18:09 +0530

    created templates
    i've created urls, views, and html tamplates

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from brokennest.views import *
 
 urlpatterns = [
+    path("", notebook, name = "notebook"),
     path("admin/", admin.site.urls),
 ]
