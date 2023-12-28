3bf0da20ee8102c9fd4acb53d583cd2bb9e3afd7
AdarshVarma9395 <adarshvarma1999@gmail.com>
2023-12-28 17:32:29 +0530

    Crud Operations
    i've performed post method and learned how to get data from frontend to DB, how to fetch record from the corresponding database, how to create objects in django models etc.

--- brokennest/views.py
+++ brokennest/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import *
+
 
 def notebook(request):
-    return render(request, "books.html")
+    if request.method == "POST":
+        data = request.POST
+
+        note_name = data.get("note_name")
+        note_description = data.get("note_description")
+
+        books.objects.create(
+            note_name = note_name,
+            note_description = note_description,
+        )
+
+        return redirect("notebook")
+
+    queryset = books.objects.all()
+    context = {"notebook":queryset}
+
+
+    return render(request, "books.html", context)
